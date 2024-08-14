e323a4142454f74e71394551ad77176292be441a
Dzmitry Kishylau <dzmitry@retool.com>
2024-08-14 11:20:23 -0700

    [chore] Prepare the repo for publishing to the Terraform Registry

--- main.go
+++ main.go
@@ -38,8 +38,6 @@ func main() {
 
 	opts := providerserver.ServeOpts{
 		// TODO(dzmitry.kishylau): Update this string with the published name of Retool provider.
-		// Also update the tfplugindocs generate command to either remove the
-		// -provider-name flag or set its value to the updated provider name.
 		Address: "registry.terraform.io/retool/retool",
 		Debug:   debug,
 	}
