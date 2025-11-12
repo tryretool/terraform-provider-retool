3e517a6927f9356f750c0340a1471bdc09cbdef7
Luke Wright <lukew@retool.com>
2025-11-12 14:24:14 -0800

    [feat][gov] Add ability to manage resources (#55)
    * update repo to reflect v.2.9.0 of the api
    
    * fixes to cursor slop
    
    * notes in readme
    
    * all tests passing, working on a local server with real examples
    
    * docs
    
    * lint fixes
    
    * add comment on folder deletion recursion
    
    * notes on reviewing
    
    * better handling for config variables as secret
    
    * update docs
    
    * lint
    
    * lint
    
    * add resources resource
    
    * docs
    
    * fix test
    
    * lint
    
    * lint
    
    * lint
    
    * docs update
    
    * more accurate docs

---
+++ examples/resources/retool_resource/import.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Import a Retool resource using its UUID
+# Note: The options field will not be imported and must be set in your configuration
+terraform import retool_resource.example "resource_uuid_here"
+
